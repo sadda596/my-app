191b9157c3c314f5f9ef0620dd4c7d548f2eccfa
Sadda Dinesh Kumar <sadda.dinesh.kumar@ibm.com>
2022-07-25 12:59:07 +0530

    add all the new files

---
+++ src/main/java/com/example/java_batch/JavaBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_batch/JavaBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
